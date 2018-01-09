39296064fd6406537784e67da8aebfa6df1479a9
Mathia Van De Poel <mathia.vandepoel@gmail.com>
2018-01-09 15:53:22 +0100

    more renames

---
+++ RavenTestConsole/IRavenDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace RavenTestConsole
+{
+    public interface IRavenDatabase : IDisposable
+    {
+      
+        IDocumentSession GetSession();
+        IDocumentStore Store { get; }
+    }
+}
---
+++ RavenTestConsole/RavenDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace RavenTestConsole
+{
+	public class RavenDatabase : IRavenDatabase
+	{
+		public IDocumentStore Store { get; private set; }
+
+		public RavenDatabase()
+		{
+			Store = new DocumentStore
+			{
+				Urls = new[] {"http://localhost:8080/"},
+				Database = "zoo"    
+			};
+			Store.Initialize();
+		}
+
+		public void Dispose()
+		{
+			Store.Dispose();
+		}
+
+		public IDocumentSession GetSession()
+		{
+			return Store.OpenSession();
+		}
+	}
+}
